2cfea9473cd3b5b758258185c7b07a61e68c2a46
AndySouth <southandy@gmail.com>
2015-07-30 18:42:57 +0100

    add a change popn function to do what currently in server.r

---
+++ R/change_pop.r
@@ -0,0 +1,37 @@
+#' change vector population
+#'
+#' simple logistic model to change vector population based on growth rate, K, insecticide and resistance
+#'
+#' @param pop vector population in this timestep
+#' @param rateGrowth population growth rate
+#' @param carryCap carrying capacity (K) in the logistic model
+#' @param rateInsecticideKill kill rate due to insecticide
+#' @param rateResistance effect on resistance on insecticide kill rate
+#' @param resistanceModifier modifies effect of resistance
+#' @examples
+#' change_pop(pop=0.5, rateGrowth=0.4, carryCap=1, rateInsecticideKill=0.4, rateResistance=0.2, resistanceModifier=1)
+#' @return float population in next timestep
+#' @export
+
+change_pop <- function(pop,
+                       rateGrowth,
+                       carryCap,
+                       rateInsecticideKill,
+                       rateResistance,
+                       resistanceModifier
+) 
+{
+  
+  pop2 <- pop +
+          rateGrowth * pop * (1-pop/carryCap) -
+          rateInsecticideKill * pop *
+          (1-rateResistance ^ (1/resistanceModifier) )
+  
+  return(pop2)
+  
+#   dF$vectorPop[runNum+1] <<- dF$vectorPop[runNum] + 
+#     rateGrowth * dF$vectorPop[runNum] * (1-dF$vectorPop[runNum]/K) -   #density dependence
+#     rateInsecticideKill * dF$vectorPop[runNum] *                       #insecticide
+#     (1-dF$pyrResist[runNum]^(1/resistanceModifier))                    #resistance  
+  
+}
