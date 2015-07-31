fe389fefcf51f1b35f71d16784f12c79b7d95205
AndySouth <southandy@gmail.com>
2015-07-31 16:10:15 +0100

    change in resistance over one timestep

---
+++ R/change_resistance.r
@@ -0,0 +1,48 @@
+#' change resistance rate
+#'
+#' change resistance to one insecticide
+#'
+#' @param resistance resistance rate in this timestep
+#' @param resist_incr increase in resistance in presence of insecticide
+#' @param resist_decr decrease in resistance in absence of insecticide
+# @param insecticide_on whether insecticide is applied 0=no, 1=yes
+#' @param resistance_on whether the insecticide applied prompts resistance 0=no, 1=yes
+#' @examples
+#' change_resistance(resistance=0.2, resist_incr=0.4, resist_decr=0.2, resistance_on=1)
+#' @return float resistance in next timestep
+#' @export
+
+change_resistance <- function(resistance,
+                              resist_incr,
+                              resist_decr,
+                              resistance_on
+) 
+{
+  
+  #todo do I want to put numTimesteps as an arg to allow it to run & return a vector ? or have as other func.
+  
+  resistance2 <- NULL
+  
+  if ( resistance_on )
+  {
+    
+    resistance2 <- resistance + resist_incr * (1-resistance)    
+    
+  } else
+  {
+    
+    resistance2 <- resistance * (1-resist_decr) 
+    
+  }
+  
+  
+  return(resistance2)
+  
+#   if ( input$pyrOn || input$ddtOn ) 
+#     #constraining resistance to 1
+#     dF$pyrResist[runNum+1] <<- dF$pyrResist[runNum] +
+#       rateResistance * (1 - dF$pyrResist[runNum])
+#   else 
+#     dF$pyrResist[runNum+1] <<- dF$pyrResist[runNum] * (1-rateResistance)  
+  
+}
