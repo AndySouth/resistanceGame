7a11cf83e734f9c4cd21737af9d6c6db8765859a
AndySouth <southandy@gmail.com>
2015-07-31 14:19:31 +0100

    rename args

--- R/change_pop.r
+++ R/change_pop.r
@@ -3,38 +3,62 @@
 #' simple logistic model to change vector population based on growth rate, K, insecticide and resistance
 #'
 #' @param pop vector population in this timestep
-#' @param rateGrowth population growth rate
-#' @param carryCap carrying capacity (K) in the logistic model
-#' @param rateInsecticideKill kill rate due to insecticide
-#' @param rateResistance effect on resistance on insecticide kill rate
-#' @param resistanceModifier modifies effect of resistance
+#' @param rate_growth population growth rate
+#' @param carry_cap carrying capacity (K) in the logistic model
+#' @param rate_insecticide_kill kill rate due to insecticide
+#' @param rate_resistance effect on resistance on insecticide kill rate
+#' @param resistance_modifier modifies effect of resistance
+#' @param insecticide_on whether insecticide is applied 0=no, 1=yes
+#' @param resistance_on whether there is resistance to the applied insecticide 0=no, 1=yes
 #' @examples
-#' change_pop(pop=0.5, rateGrowth=0.4, carryCap=1, rateInsecticideKill=0.4, rateResistance=0.2, resistanceModifier=1)
+#' change_pop(pop=0.5, rate_growth=0.4, carry_cap=1, rate_insecticide_kill=0.4, rate_resistance=0.2, resistance_modifier=1)
 #' @return float population in next timestep
 #' @export
 
 change_pop <- function(pop,
-                       rateGrowth,
-                       carryCap,
-                       rateInsecticideKill,
-                       rateResistance,
-                       resistanceModifier
+                       rate_growth,
+                       carry_cap,
+                       rate_insecticide_kill,
+                       rate_resistance,
+                       resistance_modifier,
+                       insecticide_on,
+                       resistance_on
 ) 
 {
   
   #todo do I want to put numTimesteps as an arg to allow it to run & return a vector ? or have as other func.
-  #todo do I want T/F args for insecticideOn & resistanceOn
+
+  pop2 <- NULL
   
-  pop2 <- pop +
-          rateGrowth * pop * (1-pop/carryCap) -
-          rateInsecticideKill * pop *
-          (1-rateResistance ^ (1/resistanceModifier) )
+  if ( insecticide_on && resistance_on )
+  {
+    
+    pop2 <- pop +
+      rate_growth * pop * (1-pop/carry_cap) -
+      rate_insecticide_kill * pop *
+      (1-rate_resistance ^ (1/resistance_modifier) )    
+    
+  } else if ( insecticide_on )
+  {
+    
+    pop2 <- pop +
+      rate_growth * pop * (1-pop/carry_cap) -
+      rate_insecticide_kill * pop 
+    
+  } else
+  {
+    
+    pop2 <- pop +
+      rate_growth * pop * (1-pop/carry_cap) 
+    
+  }
+  
   
   return(pop2)
   
 #   dF$vectorPop[runNum+1] <<- dF$vectorPop[runNum] + 
-#     rateGrowth * dF$vectorPop[runNum] * (1-dF$vectorPop[runNum]/K) -   #density dependence
-#     rateInsecticideKill * dF$vectorPop[runNum] *                       #insecticide
-#     (1-dF$pyrResist[runNum]^(1/resistanceModifier))                    #resistance  
+#     rate_growth * dF$vectorPop[runNum] * (1-dF$vectorPop[runNum]/K) -   #density dependence
+#     rate_insecticide_kill * dF$vectorPop[runNum] *                       #insecticide
+#     (1-dF$pyrResist[runNum]^(1/resistance_modifier))                    #resistance  
   
 }
