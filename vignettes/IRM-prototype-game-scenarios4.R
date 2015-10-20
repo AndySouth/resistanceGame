34f7303a646ba06f011873e570e696ed9d8e55a2
AndySouth <southandy@gmail.com>
2015-10-20 16:47:55 +0100

    modify vignettes

--- vignettes/IRM-prototype-game-scenarios4.R
+++ vignettes/IRM-prototype-game-scenarios4.R
@@ -3,51 +3,51 @@
 require(resistanceGame)
 
 ## ---- eval=TRUE, echo=TRUE, message=FALSE, results='hide', fig.width=7, fig.height=6----
-plot_sim( run_sim() )
+plot_sim( run_sim(num_tsteps=360, resist_incr=0.02, resist_decr = 0.01) )
 
 ## ---- eval=TRUE, echo=TRUE, message=FALSE, results='hide', fig.width=7, fig.height=6----
 l_config <- read_config()
-l_config2 <- config_plan(l_config, t_strt=1, t_stop=20, control_id='irs_pyr')
-plot_sim( run_sim(l_config=l_config2) )
+l_config2 <- config_plan(l_config, t_strt=1, t_stop=360, control_id='irs_pyr')
+plot_sim( run_sim(l_config=l_config2, num_tsteps=360, resist_incr=0.02, resist_decr = 0.01) )
 
 ## ---- eval=TRUE, echo=TRUE, message=FALSE, results='hide', fig.width=7, fig.height=6----
-l_config2 <- config_plan(l_config, t_strt=1, t_stop=20, control_id='irs_ops')
-plot_sim( run_sim(l_config=l_config2) )
+l_config2 <- config_plan(l_config, t_strt=1, t_stop=360, control_id='irs_ops')
+plot_sim( run_sim(l_config=l_config2, num_tsteps=360, resist_incr=0.02, resist_decr = 0.01) )
 
 ## ---- eval=TRUE, echo=TRUE, message=FALSE, results='hide', fig.width=7, fig.height=6----
-l_config2 <- config_plan(l_config, t_strt=seq(1,20,2), t_stop=seq(1,20,2),
+l_config2 <- config_plan(l_config, t_strt=1, t_stop=180,
                             control_id='irs_pyr')
-plot_sim( run_sim(l_config=l_config2) )
+plot_sim( run_sim(l_config=l_config2, num_tsteps=360, resist_incr=0.02, resist_decr = 0.01) )
 
 ## ---- eval=TRUE, echo=TRUE, message=FALSE, results='hide', fig.width=7, fig.height=6----
-l_config2 <- config_plan(l_config, t_strt=seq(1,20), t_stop=seq(1,20), control_id=c('irs_pyr','irs_ops'))
-plot_sim( run_sim(l_config=l_config2) )
+l_config2 <- config_plan(l_config, t_strt=1, t_stop=180, control_id=c('irs_ops'))
+plot_sim( run_sim(l_config=l_config2, num_tsteps=360, resist_incr=0.02, resist_decr = 0.01) )
 
 ## ---- eval=TRUE, echo=TRUE, message=FALSE, results='hide', fig.width=7, fig.height=6----
-l_config2 <- config_plan(l_config, t_strt=c(1,11), t_stop=c(10,20),
+l_config2 <- config_plan(l_config, t_strt=c(1,181), t_stop=c(180,360),
                             control_id=c('irs_pyr','irs_ops'))
-plot_sim( run_sim(l_config=l_config2) )
+plot_sim( run_sim(l_config=l_config2, num_tsteps=360, resist_incr=0.02, resist_decr = 0.01) )
 
 ## ---- eval=TRUE, echo=TRUE, message=FALSE, results='hide', fig.width=7, fig.height=6----
-l_config2 <- config_plan(l_config, t_strt=c(1,11), t_stop=c(10,20),
+l_config2 <- config_plan(l_config, t_strt=c(1,181), t_stop=c(180,360),
                             control_id=c('irs_pyr','irs_ddt'))
-plot_sim( run_sim(l_config=l_config2) )
+plot_sim( run_sim(l_config=l_config2, num_tsteps=360, resist_incr=0.02, resist_decr = 0.01) )
 
 ## ---- eval=TRUE, echo=TRUE, message=FALSE, results='hide', fig.width=7, fig.height=6----
-plot_sim( run_sim(randomness=0.5) )
+plot_sim( run_sim(randomness=0.5, num_tsteps=360, resist_incr=0.02, resist_decr = 0.01) )
 
 ## ---- eval=TRUE, echo=TRUE, message=FALSE, results='hide', fig.width=7, fig.height=6----
-plot_sim( run_sim(randomness=0.5) )
+plot_sim( run_sim(randomness=0.5, num_tsteps=360, resist_incr=0.02, resist_decr = 0.01) )
 
 ## ---- eval=TRUE, echo=TRUE, message=FALSE, results='hide', fig.width=7, fig.height=6----
-plot_sim( run_sim(randomness=0.1) )
+plot_sim( run_sim(randomness=0.1, num_tsteps=360, resist_incr=0.02, resist_decr = 0.01) )
 
 ## ---- eval=TRUE, echo=TRUE, message=FALSE, results='hide', fig.width=7, fig.height=6----
-l_config2 <- config_plan(l_config, t_strt=1, t_stop=20, control_id='irs_pyr')
-plot_sim( run_sim(l_config=l_config2, resistance_modifier=0.1) )
+l_config2 <- config_plan(l_config, t_strt=1, t_stop=360, control_id='irs_pyr')
+plot_sim( run_sim(l_config=l_config2, resistance_modifier=0.1, num_tsteps=360, resist_incr=0.02, resist_decr = 0.01) )
 
 ## ---- eval=TRUE, echo=TRUE, message=FALSE, results='hide', fig.width=7, fig.height=6----
-plot_sim( run_sim(l_config=l_config2, resistance_modifier=10) )
+plot_sim( run_sim(l_config=l_config2, resistance_modifier=10, num_tsteps=360, resist_incr=0.02, resist_decr = 0.01) )
 
 ## ---- eval=TRUE, echo=TRUE, message=FALSE, results='hide', fig.width=7, fig.height=6----
 l_config2 <- config_controls(l_config, control_id='new_ai', vector_id='an_gamb',
@@ -56,21 +56,24 @@ l_config2 <- config_resistances(l_config2, control_id='new_ai', resistance_id="t
                                 resistance_strength=0.8,
                                 resistance_incr=0.2,
                                 resistance_decr=0.1)
-l_config2 <- config_plan(l_config2, t_strt=1, t_stop=20, control_id='new_ai')
-plot_sim( run_sim(l_config=l_config2) )
+l_config2 <- config_plan(l_config2, t_strt=1, t_stop=360, control_id='new_ai')
+plot_sim( run_sim(l_config=l_config2, num_tsteps=360, resist_incr=0.02, resist_decr = 0.01) )
 
-
-## ---- eval=TRUE, echo=TRUE, message=FALSE, results='hide', fig.width=7, fig.height=6----
-emergence = c(rep(0.5,6),rep(0,6))
-plot_sim( run_sim(emergence=emergence, survival=0.7), plot_emergence=TRUE )
-
-## ---- eval=TRUE, echo=TRUE, message=FALSE, results='hide', fig.width=7, fig.height=6----
-emergence = seq(0.5,0,-0.05)
-plot_sim( run_sim(emergence=emergence, survival=0.7), plot_emergence=TRUE )
 
 ## ---- eval=TRUE, echo=TRUE, message=FALSE, results='hide', fig.width=7, fig.height=6----
 emergence = c(rep(0.3,180),rep(0,180))
-plot_sim( run_sim(num_tsteps=360,emergence=emergence, survival=0.7), plot_emergence=TRUE )
+plot_sim( run_sim(emergence=emergence, num_tsteps=360, resist_incr=0.02, resist_decr = 0.01), plot_emergence=TRUE )
+
+## ---- eval=TRUE, echo=TRUE, message=FALSE, results='hide', fig.width=7, fig.height=6----
+#sin curve to create a gradual change from 0 to 0.3
+emergence = 0.3*sin(seq(0,3.1,0.01))
+plot_sim( run_sim(emergence=emergence, num_tsteps=360, resist_incr=0.02, resist_decr = 0.01), plot_emergence=TRUE )
+
+## ---- eval=TRUE, echo=TRUE, message=FALSE, results='hide', fig.width=7, fig.height=6----
+emergence = c(rep(0.3,180),rep(0,180))
+plot_sim( run_sim(num_tsteps=360,emergence=emergence, survival=0.7,
+                  resist_incr=0.02, resist_decr = 0.01), 
+          plot_emergence=TRUE )
 
 ## ---- eval=TRUE, echo=TRUE, message=FALSE, results='hide', fig.width=7, fig.height=6----
 emergence = c(rep(0.3,180),rep(0,180))
