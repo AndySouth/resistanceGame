f7f76200d2e6cbf5ee0b61c41f2f1ec028f5c9ca
AndySouth <southandy@gmail.com>
2018-01-24 12:29:09 +0000

    old tweaks in reading data

--- R/vecbyloc_from_gdocs.R
+++ R/vecbyloc_from_gdocs.R
@@ -13,12 +13,14 @@ vecbyloc_from_gdocs <- function()
   #uses locations & vectors dataframes which are read from googledocs config files & stored in package
   
   emers <- data.frame(stringsAsFactors=FALSE)
-  emer_strings <- NULL  
+  emer_strings <- NULL 
+  loc_vec_res_ids <- NULL
+  emers$vec_res_ids <- NULL
   
   #for each location
-  for (i in 1:nrow(locations))
+  for (loc_num in 1:nrow(locations))
   {
-    tmp <- locations$emergences[i]
+    tmp <- locations$emergences[loc_num]
     
     #remove comma from middle and split in two
     tmp2 <- unlist(strsplit(tmp, split = ","))
@@ -27,26 +29,46 @@ vecbyloc_from_gdocs <- function()
     tmp4 <- gsub('\\)','',tmp3)
     
     #this might not work if just one vector
-    vec_ids <- unlist(strsplit(locations$`vec_ids`[i], split = ","))
+    vec_ids <- unlist(strsplit(locations$`vec_ids`[loc_num], split = ","))
     
-    #for each vector at this location
-    for (j in 1:length(tmp4))
+    #sort emergences for each vector at this location
+    for (vec_num in 1:length(vec_ids))
     {
       #expanding the string
-      tmp5 <- expand_season(tmp4[j], return_tstep = 'months')
+      tmp5 <- expand_season(tmp4[vec_num], return_tstep = 'months')
       
       #cat(tmp5,"\n")
       #add string itself onto vector, causes factor problems
-      #tmp5 <- c(tmp4[j], tmp5)
-      emer_strings <- c(emer_strings, tmp4[j]) 
+      emer_strings <- c(emer_strings, tmp4[vec_num]) 
       
       emers <- rbind(emers, tmp5) #, stringsAsFactors=FALSE)
 
-      #add string itself onto vector
-      #emers$emer_str <- tmp4[j]
+      #add string itself
+      emers$emer_string <- emer_strings
             
-      rownames(emers)[nrow(emers)] <- paste0(locations$loc_id[i],"_",vec_ids[j])
-    }    
+      rownames(emers)[nrow(emers)] <- paste0(locations$loc_id[loc_num],"_",vec_ids[vec_num])
+    }  
+    
+    
+    #*****sort how to get resistanceIDs and freqs onto this file
+    #*****without making it too ugly !!
+    
+    #sort resistance IDs & frequencies for each vec at this loc
+    #e.g. (0.7);(0.3,0.7), but beware there can be multiple resistances per vector
+    #so may not be possible to get this to work perfectly ...
+    for (vec_num in 1:length(vec_ids))
+    {
+      #this might not work if just one vector
+      vec_res_ids <- unlist(strsplit(locations$`vec_res_ids`[loc_num], split = ";"))
+      #remove brackets
+      vec_res_ids <- gsub('\\(','',vec_res_ids)
+      vec_res_ids <- gsub('\\)','',vec_res_ids)
+      
+      #loc_vec_res_ids <- c(loc_vec_res_ids,vec_res_ids)
+      emers$vec_res_ids[] <- loc_vec_res_ids
+    }
+    emers$vec_res_ids <- loc_vec_res_ids    
+    
     
   }
   colnames(emers) <- paste0("m",c(1:ncol(emers)))
@@ -58,7 +80,10 @@ vecbyloc_from_gdocs <- function()
   #vectors is read from gdocs & sored in package
   emers$survival <- vectors$survival[ match( emers$vec_id, vectors$vec_id) ]
   
-  emers$emer_string <- emer_strings
+
+  
+  #add to file the 
+  
   
   return(emers)
 
